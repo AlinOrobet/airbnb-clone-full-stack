6435db68c6222239535105da6aea66ed21cfd1fb
AlinOrobet <alin.orobet@student.usv.ro^C>
2023-04-03 22:25:03 +0300

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import {Nunito} from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Airbnb",
@@ -15,7 +18,11 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
